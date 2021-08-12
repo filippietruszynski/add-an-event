980845dc59cd4c6e3094736966411b5da80e604e
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-08-12 16:31:20 +0200

    [backend] Relocate dotenv require from datadase to package.json

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-require("dotenv").config();
-
 export const connect = (url = process.env.DB_URI_DEV, opts = {}) => {
   return mongoose.connect(url, {
     ...opts,
